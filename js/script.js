475fb6ae5d16e553a875a8888b9f560875f73d43
Sergey <sergekotov95@gmail.com>
2021-01-13 14:02:47 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+"use strict"; 
+
+// const category = 'toys'; 
+
+// console.log(`https://sumurl.com/${category}`);
+
+// const user = "Ivan"; 
+// // alert(`Privet ${user}`); 
+
+let incr = 10,
+    decr = 10; 
+
+
+console.log(incr++);
+console.log(decr--); 
+
+const isChecked = true,
+      isClosed = true; 
+
+console.log(isChecked && isClosed); 
+
