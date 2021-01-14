eeb13ee57e0d4d5e62697d7eab601b1ff5584172
Sergey <sergekotov95@gmail.com>
2021-01-14 11:47:30 +0300

    Practice part 3

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 "use strict"; 
 
-let numberOfFilms = +prompt('Skolko filmov yzhe posmotreli?', '');
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('Skolko filmov yzhe posmotreli?', '');
+
+    while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Skolko filmov yzhe posmotreli?', '');
+    }
+}
+
+start(); 
 
 let personalMovieDB = {
     count: numberOfFilms,
@@ -10,29 +20,51 @@ let personalMovieDB = {
     privat: false
 };
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt('Odin is poslednih filmov?', '');
-    const b = prompt('Otsenka?', ''); 
-
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('done'); 
-    } else {
-        console.log('Error');
-        i--; 
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Odin is poslednih filmov?', '');
+        const b = prompt('Otsenka?', ''); 
+    
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done'); 
+        } else {
+            console.log('Error');
+            i--; 
+        }
     }
 }
 
-if (personalMovieDB.count < 10) {
-    console.log('Prosmotreno dovolno malo filmov');
-} else if (personalMovieDB.count >= 10 &&
-           personalMovieDB.count <=30) {
-    console.log('Vy klassicheskij zritel'); 
-} else if (personalMovieDB.count > 30) {
-    console.log('Vy kinoman');
-} else {
-    console.log('Error'); 
+// rememberMyFilms(); 
+
+function detectMyPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log('Prosmotreno dovolno malo filmov');
+    } else if (personalMovieDB.count >= 10 &&
+               personalMovieDB.count <=30) {
+        console.log('Vy klassicheskij zritel'); 
+    } else if (personalMovieDB.count > 30) {
+        console.log('Vy kinoman');
+    } else {
+        console.log('Error'); 
+    }
 }
 
+// detectMyPersonalLevel(); 
 
+function showMyDB(property) {
+    if(!property) {
+        console.log(personalMovieDB); 
+    }
+}
 
+showMyDB(personalMovieDB.privat); 
+
+function writeYourGenres() {
+    for (let i = 1; i < 4; i++) {
+        let genre = prompt(`Ваш любимый жанр под номером ${i}`, ''); 
+        personalMovieDB.genres.push(genre); 
+    }
+}
+
+writeYourGenres(); 
